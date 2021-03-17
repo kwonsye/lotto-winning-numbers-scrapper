b8105a1937d8141b7bd4544a5f66d9a103e7153f
kwonsye <kwonsye@naver.com>
2021-03-17 12:41:08 +0900

    [Feat] 스프링 부트 프로젝트 초기 세팅 - springboot 2.4.3 , build tool : gradle - dependency   - 크롤링을 위한 jsoup   - lombok, swagger 3.0, junit5

---
+++ src/main/java/data/lotto/scraping/LottoScrapingApplication.java
@@ -0,0 +1,11 @@
+package data.lotto.scraping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoScrapingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LottoScrapingApplication.class, args);
+    }
+}
